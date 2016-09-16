639c993cc1fa65abf8dbee731d4d3485e31d9886
jorge <jorge.alfmur@gmail.com>
2016-09-16 16:29:19 -0500

    creamos la vista del dashboard con los botones y la route pero AUN no damos la funcionalidad del boton publicar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :categories
-  devise_for :users
 
   #las rutas de commets depende del article
   resources :articles do
   	resources :comments, only: [:create, :destroy, :update]
   end
   
+  devise_for :users
   root 'welcome#index'
+
+  get '/dashboard', to: 'welcome#dashboard'
 end
